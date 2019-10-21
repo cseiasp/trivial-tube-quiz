e1c455e3b7f68de40b14c73833a401655a64201f
Liko <li_xie1992@hotmail.com>
2019-10-21 10:42:19 +0100

    added some seed data and associations

---
+++ db/seeds.rb
@@ -0,0 +1,33 @@
+require_relative '../config/environment'
+
+Station.create(name: "Moorgate")
+Station.create(name: "Embankment")
+Station.create(name: "Notting Hill Gate")
+Station.create(name: "Edgware Road")
+Station.create(name: "Mile End")
+
+Line.create(colour: "Black", name: "Northern")
+Line.create(colour: "Brown", name: "Bakerloo")
+Line.create(colour: "Yellow", name: "Circle")
+Line.create(colour: "Pink", name: "Hammersmith & City")
+Line.create(colour: "Grey", name: "Jubilee")
+Line.create(colour: "Light Blue", name: "Victoria")
+Line.create(colour: "Red", name: "Central")
+Line.create(colour: "Green", name: "District")
+Line.create(colour: "Purple", name: "Metropolitan")
+Line.create(colour: "Dark Blue", name: "Piccadilly")
+Line.create(colour: "Turquoise", name: "Waterloo & City")
+
+Stop.create(station_id: 1 , line_id: 1)
+Stop.create(station_id: 2 , line_id: 2)
+Stop.create(station_id: 3 , line_id: 7)
+Stop.create(station_id: 4 , line_id: 3)
+Stop.create(station_id: 5 , line_id: 7)
+Stop.create(station_id: 1 , line_id: 4)
+Stop.create(station_id: 2 , line_id: 3)
+Stop.create(station_id: 2 , line_id: 1)
+Stop.create(station_id: 3 , line_id: 3)
+Stop.create(station_id: 3 , line_id: 8)
+
+User.create(username: "Li")
+User.create(username: "Caroline")
