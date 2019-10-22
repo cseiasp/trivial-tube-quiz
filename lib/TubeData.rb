02da839e128127b2ffbb4daabdb3bcb4d373c30a
Liko <li_xie1992@hotmail.com>
2019-10-22 11:54:43 +0100

    supposedly fixed seed issues

--- lib/TubeData.rb
+++ lib/TubeData.rb
@@ -12,13 +12,13 @@ class TubeData
     # map through imported table to create new Station.create commands to be seeded into table
     # want to return a list of Station.creates, then use rake db:seed
     def self.seed_data_zones(path)
-        import_data(path).map {|sl| Station.create(name: sl["name"], zone: sl["zone"])}
+        import_data(path).map {|sl| Station.create(name: sl["name"].strip, zone: sl["zone"])}
     end
     
     def self.seed_data_lines(path)
         
         import_data(path).map do |sl| 
-            station_id = Station.find_by(name: sl["Name"]).id
+            station_id = Station.find_by(name: sl["Name"].strip).id
             line_id = Line.find_by( name:sl["Line"]).id
             Stop.create(station_id: station_id, line_id: line_id)
         end
