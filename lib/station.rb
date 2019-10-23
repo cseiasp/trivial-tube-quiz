bd7c22465888383b279bbc6ce6e17e309c904319
Liko <li_xie1992@hotmail.com>
2019-10-23 15:59:42 +0100

    fixed up station.rb to get rid of bad code and used previously redundant code

--- lib/station.rb
+++ lib/station.rb
@@ -2,50 +2,32 @@ class Station < ActiveRecord::Base
     has_many :stops
     has_many :lines, through: :stops
 
-
-    # Create an array of station names starting with a particular letter
     def self.stations_by_letter(letter)
-        #return array of instances stations begining with letter
         Station.all.select{|station| station.name[0] == letter}
     end
   
-
     def self.station_names_by_letter(letter)
-        #return array of station names begining with letter
         stations_by_letter(letter).map{|station| station.name}
     end
 
-
-
-    # Methods for creating an array of stations on a particular line, by id
-
-    # Create an array of station instances on a line by id
     def self.all_stations_by_line_id(line_id)
         Stop.all.select {|stop| stop.line_id == line_id}.flatten
     end
 
-    # Create an array of station ids on a particular line
     def self.station_id_array_by_line(line_id)
         all_stations_by_line_id(line_id).map {|stop| stop.station_id}
     end
 
-    #Create an array of station names on a particular line
     def self.station_names_on_line(line_id)
         station_id_array_by_line(line_id).map {|station_id| Station.find_by(id:station_id).name}
     end
 
-
-    # Creates and array of station begining with with a particular letter on a particular line
     def self.stations_on_line_by_letter(letter)
-        
         station = Station.stations_by_letter(letter).sample
-        line = station.lines.sample.name
-        
-    
-        #TODO break this up into useful methods
-        possible_stations = Line.find_by(name: line).stops.map{|stop|stop.station_id}.map{|station_id| Station.find_by(id: station_id)}.map{|station|station.name}.select{|station|station[0]==letter}
+        line = station.lines.sample
+        possible_stations = station_names_on_line(line.id).select{|station|station[0]==letter}
 
-        pair = [possible_stations, line]
+        pair = [possible_stations, line.name]
     end
     
     def self.zone_of_station(station)
