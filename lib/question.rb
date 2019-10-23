4a674ffaa3d6238bf7d83961902835559bbdc47a
Liko <li_xie1992@hotmail.com>
2019-10-23 16:57:51 +0100

    refactored checkQuestion in question.rb

--- lib/question.rb
+++ lib/question.rb
@@ -3,91 +3,8 @@ require 'pry'
 
 class Question 
 
-
-    def self.which_line_is_station_on
-        station = Station.all.sample
-
-        question = "Which line is #{station.name} on?"
-        answer = Line.lines_names_of_station(station.id)
-        hints = Hint.generate_line_hint(answer)
-
-        check = QuestionChecker.new(question, answer, hints)
-        check.compare_answers
-
-    end
-
-    def self.which_line_has_more_stations
-        line_1 = Line.all.sample
-        line_2 = (Line.all - [line_1]).sample
-
-        question = "Which of these two lines has the most stops? #{line_1.name} or #{line_2.name}"
-        answer = [Line.most_stops(line_1, line_2)]
-        
-        check = QuestionChecker.new(question, answer)
-        check.compare_answers
-    end
-
-    def self.how_many_lines_go_through
-        station = Station.all.sample
-
-        question = "How many lines go through #{station.name}?"
-        answer = [station.lines.size.to_s]
-        hints = Hint.generate_numerical_hint(answer)
- 
-        check = QuestionChecker.new(question, answer, hints)
-        check.compare_answers
-    end
-
-    def self.station_beginning_with_x
-        station = Station.all.sample
-        letter = station.name[0]
-
-        question = "Name a Station beginning with #{letter}"
-        answer = Station.station_names_by_letter(letter)
-        hints = Hint.generate_begin_with_hint(answer)
-        
-        check = QuestionChecker.new(question, answer, hints)
-        check.compare_answers
-    end
-
-    def self.station_beginning_with_x_on_line_y
-        station = Station.all.sample
-        letter = station.name[0]
-        
-        answer_array = Station.stations_on_line_by_letter(letter)
-        answer = answer_array[0]
-        line = answer_array[1]
-        question = "Name a Station beginning with #{letter} on the #{line} line."
-        hints = Hint.generate_begin_with_hint(answer)
-
-        check = QuestionChecker.new(question, answer, hints)
-        check.compare_answers
-    end
-
-    def self.which_zone_is_x_station_in
-        station = Station.all.sample
-
-        question = "Which zone is #{station.name} in?"
-        answer = Station.zone_of_station(station)
-        hints = Hint.generate_numerical_hint(answer)
-
-        check = QuestionChecker.new(question, answer, hints)
-        check.compare_answers
-    end
-
-    def self.what_colour_is_this_line?
-        line = Line.all.sample
-
-        question = "What colour is the #{line.name} line? "
-        answer = [line.colour]
-        hints = ["Black,", "Brown,", "Yellow,", "Pink,", "Grey,", "Light Blue,", "Dark Blue,", "Red,", "Green,", "Purple or", "Turquoise"] 
-
-        check = QuestionChecker.new(question, answer, hints)
-        check.compare_answers
-    end
-
     def self.ask_random_question
-         questions.sample.call
+        questions.sample.call
     end
 
     def self.questions
@@ -102,4 +19,82 @@ class Question
             ]
     end
 
+    def self.checkQuestion(question, answer, hints)
+        check = QuestionChecker.new(question, answer, hints)
+        check.compare_answers
+    end
+
+    def self.which_line_is_station_on
+        station = Station.all.sample
+
+        question = "Which line is #{station.name} on?"
+        answer = Line.lines_names_of_station(station.id)
+        hints = Hint.generate_line_hint(answer)
+
+        checkQuestion(question, answer, hints)
+    end
+
+    def self.which_line_has_more_stations
+        line_1 = Line.all.sample
+        line_2 = (Line.all - [line_1]).sample
+
+        question = "Which of these two lines has the most stops? #{line_1.name} or #{line_2.name}"
+        answer = [Line.most_stops(line_1, line_2)]
+        
+        checkQuestion(question, answer, hints)
+    end
+
+    def self.how_many_lines_go_through
+        station = Station.all.sample
+
+        question = "How many lines go through #{station.name}?"
+        answer = [station.lines.size.to_s]
+        hints = Hint.generate_numerical_hint(answer)
+ 
+        checkQuestion(question, answer, hints)
+    end
+
+    def self.station_beginning_with_x
+        station = Station.all.sample
+        letter = station.name[0]
+
+        question = "Name a Station beginning with #{letter}"
+        answer = Station.station_names_by_letter(letter)
+        hints = Hint.generate_begin_with_hint(answer)
+        
+        checkQuestion(question, answer, hints)
+    end
+
+    def self.station_beginning_with_x_on_line_y
+        station = Station.all.sample
+        letter = station.name[0]
+        
+        answer_array = Station.stations_on_line_by_letter(letter)
+        answer = answer_array[0]
+        line = answer_array[1]
+        question = "Name a Station beginning with #{letter} on the #{line} line."
+        hints = Hint.generate_begin_with_hint(answer)
+
+        checkQuestion(question, answer, hints)
+    end
+
+    def self.which_zone_is_x_station_in
+        station = Station.all.sample
+
+        question = "Which zone is #{station.name} in?"
+        answer = Station.zone_of_station(station)
+        hints = Hint.generate_numerical_hint(answer)
+
+        checkQuestion(question, answer, hints)
+    end
+
+    def self.what_colour_is_this_line?
+        line = Line.all.sample
+
+        question = "What colour is the #{line.name} line? "
+        answer = [line.colour]
+        hints = ["Black,", "Brown,", "Yellow,", "Pink,", "Grey,", "Light Blue,", "Dark Blue,", "Red,", "Green,", "Purple or", "Turquoise"] 
+
+        checkQuestion(question, answer, hints)
+    end
 end
